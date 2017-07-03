c956fc25f3c707c4f80aedc9a19055109411d2b3
Sigitkhoerul <sigtkhoerul@gmail.com>
2017-07-03 16:46:23 +0700

    Latihan Calculte

---
+++ app/src/main/java/com/example/sigit/calculate/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sigit.calculate;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewMessageActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sigit/calculate/NewMessageActivity.java
@@ -0,0 +1,8 @@
+package com.example.sigit.calculate;
+
+/**
+ * Created by SIGIT on 7/3/2017.
+ */
+
+public class NewMessageActivity {
+}
